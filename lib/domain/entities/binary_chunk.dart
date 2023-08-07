14b7a455351ef04a9d387bbbf513a618a746d92c
Minhal <minhalwaheed01@gmail.com>
2023-08-07 15:57:39 +0500

    getfirmware_url

--- lib/domain/entities/binary_chunk.dart
+++
@@ -1,9 +0,0 @@
-//domain/entites/binary_chunk.dart
-import 'package:flutter/services.dart';
-
-class BinaryChunk {
-  final int packageNumber;
-  final Uint8List chunkData;
-
-  BinaryChunk(this.packageNumber, this.chunkData);
-}
